4f2ecad01513d26fd425b1daf58287d66070304d
Adam Recvlohe <adam.recvlohe@gmail.com>
2020-12-28 22:07:34 -0500

    Add new locations

--- source/ContentScript/placeNames.ts
+++ source/ContentScript/placeNames.ts
@@ -17,6 +17,19 @@ map.set("Tulsa", "Tvlse");
 map.set("Okfuskee", "Okfvske");
 map.set("Arkansas", "Akensv");
 map.set("Okmulgee", "Okmvlke");
+map.set("Atlanta", "Pvkvn Huere");
+map.set("Chattahoochee River", "Cvto-Hocce Hvcce");
+map.set("Conecuh River", "Koha Anaka");
+map.set("Etowah", "Etvlwv");
+map.set("North Canadian River", " Oktah Hvcce");
+map.set("United States", "Wvcenv Emekvnv");
+map.set("Tallahatchie River", "Tvlwv Hvcce");
+map.set("Tallahassee", "Tvlvhasse");
+map.set("Seminole", "Semvnole");
+map.set("Ogeechee River", "Okece");
+map.set("Red River", "Uecate Hvcce");
+map.set("Chattanooga", "Cvto Nuka");
+map.set("Waxahatchee", "Wase Hvcce");
 
 const tempArr = Array.from(map);
 
